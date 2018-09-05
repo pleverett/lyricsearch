08cc11673d9334ef164afaed611431214d15dae7
Paul Leverett <pleverett@gmail.com>
2018-09-05 12:00:49 -0700

    Finished Lyrics component

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import Moment from 'react-moment'
 import axios from 'axios'
 import Spinner from '../layout/Spinner'
 
@@ -14,7 +14,7 @@ class Lyrics extends Component {
     axios
       .get(
         `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
-          this.props.match.params.id
+        this.props.match.params.id
         }&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(result => {
@@ -23,7 +23,7 @@ class Lyrics extends Component {
 
         return axios.get(
           `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${
-            this.props.match.params.id
+          this.props.match.params.id
           }&apikey=${process.env.REACT_APP_MM_KEY}`
         )
       })
@@ -52,11 +52,29 @@ class Lyrics extends Component {
             Go Back
           </Link>
           <div className="card">
-            <div className="card-header">
+            <h5 className="card-header">
               {track.track_name} by{' '}
               <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
             </div>
           </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>: <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+            </li>
+          </ul>
         </React.Fragment>
       )
     }
